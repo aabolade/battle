ffaffc422dceb48c9aa1a78e07868a0755fe2274
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-11-23 11:35:02 +0000

    view shows player 2's hit points

--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -1,6 +1,3 @@
-require 'capybara/dsl'
-require 'selenium-webdriver'
-include Capybara::DSL
 
 Capybara.default_driver = :selenium
 
@@ -15,5 +12,13 @@ RSpec.feature "Battle Names", type: :feature do
     click_button('Submit')
 
     expect(page).to have_content('Pikachu vs. Charizard')
+  end
+end
+
+RSpec.feature "Player 2's hit points", type: :feature do
+  scenario "Show player 2's hit points" do
+    visit "/play"
+
+    expect(page).to have_content("Player 2 Score: #{100}")
   end
 end
