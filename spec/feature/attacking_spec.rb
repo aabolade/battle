400da1f3c195ccc8201d0115c765f0e8b294239d
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-11-23 12:23:26 +0000

    created page for attack

--- spec/feature/attacking_spec.rb
+++ spec/feature/attacking_spec.rb
@@ -4,10 +4,12 @@ RSpec.feature "Attacking P2", type: :feature do
   scenario "Player 1 should attack Player 2" do
 
     sign_in_and_play
+    expect(page).not_to have_content "Pikachu attacked Charizard!"
+    click_button("attack")
 
-    click_button("Attack!")
-
-    expect(page).to have_content "#{@p1name} attacked #{@p2name}!"
+    expect(page).to have_content "Pikachu attacked Charizard!"
 
   end
+
+
 end
