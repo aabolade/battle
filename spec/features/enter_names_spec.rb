a1f5f8988e92a26fd7c314d4d9db42a9ddcf90fc
Chris <chrixs99@hotmail.com>
2016-11-24 11:06:27 +0000

    successfully used test helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,10 +1,6 @@
 RSpec.feature "Submitting name", :type => :feature do
   scenario "User enters name to play" do
-    visit "/"
-
-    fill_in "P1_name", :with => "Chris"
-    fill_in "P2_name", :with => "Leke"
-    click_button('submit')
+    sign_in_and_play
 
     expect(page).to have_content("Player 1: Chris, Player 2: Leke")
   end
@@ -12,11 +8,7 @@ end
 
 RSpec.feature "Hitpoint check", :type => :feature do
   scenario "Can see other players hitpoints" do
-    visit "/"
-
-    fill_in "P1_name", :with => "Chris"
-    fill_in "P2_name", :with => "Leke"
-    click_button('submit')
+    sign_in_and_play
 
     expect(page).to have_content("Player 2 HP: 100")
   end
